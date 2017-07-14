a69b718de1873f1c35dd197fc2e76220bd26548b
charmichauhan <charmichauhan9503@gmail.com>
2017-07-14 10:35:52 +0530

    Add files via upload

---
+++ gulp/config.js
@@ -0,0 +1,44 @@
+var dest = './build';
+var src  = './src';
+
+module.exports = {
+  browserSync: {
+    server: {
+      baseDir: [dest, src]
+    },
+    files: [
+      dest + '/**',
+      '!' + dest + '/**.map'
+    ]
+  },
+  port : 4500,
+  sass: {
+    src: src + '/sass/**/*.scss',
+    dest: dest
+  },
+  production: {
+    cssSrc: dest + '/*.css',
+    jsSrc: dest + '/*.js',
+    dest: dest
+  },
+  markup: {
+    src: src + '/htdocs/*.html',
+    watch: src + '/htdocs/**',
+    dest: dest,
+    swig_options: {
+      defaults: {
+        cache: false
+      },
+      json_path: src + '/data'
+    }
+  },
+  browserify: {
+    debug: true,
+    extensions: ['.jsx'],
+    bundleConfigs: [{
+      entries: src + '/javascript/app.jsx',
+      dest: dest + '/',
+      outputName: 'app.js'
+    }]
+  }
+};
